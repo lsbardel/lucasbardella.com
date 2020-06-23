9afc20228835fe41920541cc2b995bc5a85d0ab6
Luca Sbardella <luca@quantmind.com>
2020-06-23 17:11:14 +0100

    change name

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -2,10 +2,6 @@ import { Page } from "@metablock/react";
 import React from "react";
 
 const Home = () => {
-  const changeColorOnScroll = {
-    height: 400,
-    color: "white",
-  };
   return (
     <Page>
       <div>Hi</div>
