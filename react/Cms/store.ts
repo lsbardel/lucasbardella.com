9f621d44a1f9b88ef0949a496910673404819e63
Luca Sbardella <luca@quantmind.com>
2020-06-27 15:55:12 +0100

    Remove ract pages

--- react/Cms/store.ts
+++ react/Cms/store.ts
@@ -18,7 +18,7 @@ export class CmsStore {
     this.error = undefined;
     try {
       if (!this.data[path]) {
-        const response = await this.cli.get(path);
+        const response = await this.cli.get(path, { mode: "no-cors" });
         this.data[path] = response.data;
       }
       return this.data[path];
